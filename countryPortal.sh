c750e863372b7fd3f2cb1eae1a26769685a1731c
connieyeeson <110239716+connieyeeson@users.noreply.github.com>
2023-11-01 03:29:31 +0000

    Add files via upload

--- countryPortal.sh
+++ countryPortal.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-# Welcome message
-echo "Welcome to the Country Information Program!"
+# First step! Prompt User for a little information
+echo "Please enter your name:"
+read userName
+echo "Hello, $userName, and welcome to our Portal"
 
-# Read user input
-read -p "Enter a country name to get information: " country_name
+while true; do
+    # Read user input
+    read -p "Enter a country name that you want to search (or 'exit' to quit): " country_name
 
-# Search for the country in the data file
-country_info=$(grep -i "^$country_name," countries.csv)
+    if [ "$country_name" == "exit" ]; then
+        echo "Goodbye, $userName! Have a nice day."
+        break
+    elif [ "$country_name" == "continue" ]; then
+        continue
+    fi
 
-# Check if the country was found
-if [ -n "$country_info" ]; then
-     # Display the information in a vertical format
-    echo "Information for $country_name:"
-    IFS=',' read -ra fields <<< "$country_info"
-    echo "Country: ${fields[0]}"
-    echo "Capital: ${fields[1]}"
-    echo "Current President: ${fields[2]}"
-    echo "Language: ${fields[3]}"
-    echo "Currency: ${fields[4]}"
-else
-    # Country not found or misspelled
-    echo "Country not found or misspelled. Please check the spelling and try again."
-fi
+    # Search for the country in the data file
+    country_info=$(grep -i "^$country_name," countries.csv)
+
+    if [ -n "$country_info" ]; then
+        # Display the information in a vertical format
+        echo "Information for $userName $country_name:"
+        IFS=',' read -ra fields <<< "$country_info"
+        echo "Country: ${fields[0]}"
+        echo "Capital: ${fields[1]}"
+        echo "Current President: ${fields[2]}"
+        echo "Language: ${fields[3]}"
+        echo "Currency: ${fields[4]}"
+    else
+        # Country not found or misspelled
+        echo "Country not found or misspelled. Please check the spelling and try again."
+    fi
+done
