60b8f06a88b1dd1c2d61e21928fb9c92ffd087c6
connieyeeson <110239716+connieyeeson@users.noreply.github.com>
2023-11-03 05:27:18 +0000

    Add files via upload

--- countryPortal.sh
+++ countryPortal.sh
@@ -14,33 +14,43 @@ while true; do
         continue
     fi
 
-    # Search for the country in the data file
     country_info=$(grep -i "^$country_name," countries.csv)
 
     if [ -n "$country_info" ]; then
         echo "Information for $country_name:"
-        
-        # Provide options for specific information
-        echo "Select an option to know more about this country"
-        echo "1. Capital City"
-        echo "2. Language"
-        echo "3. Currency"
-        read -p "Enter the option number (1/2/3): " option
 
-        case $option in
-            1)
-                echo "Capital City: $(echo $country_info | cut -d',' -f2)"
-                ;;
-            2)
-                echo "Language: $(echo $country_info | cut -d',' -f3)"
-                ;;
-            3)
-                echo "Currency: $(echo $country_info | cut -d',' -f4)"
-                ;;
-            *)
-                echo "Invalid option. Please select 1, 2, or 3."
-                ;;
-        esac
+        echo "Select an option to know more about $country_name"
+        echo "1. Capital City"
+        echo "2. Currency"
+        echo "3. Official Language"
+        echo "4. Head of Government"
+        echo "5. Exit"
+
+        while true; do
+            read -p "Enter the option number (1/2/3/4/5): " option
+
+            case $option in
+                1)
+                    echo "Capital City: $(echo $country_info | cut -d',' -f2)"
+                    ;;
+                2)
+                    echo "Currency: $(echo $country_info | cut -d',' -f3)"
+                    ;;
+                3)
+                    echo "Official Language: $(echo $country_info | cut -d',' -f4)"
+                    ;;
+                4)
+                    echo "Head of Government: $(echo $country_info | cut -d',' -f5)"
+                    ;;
+                5)
+                    echo "Returning to the main menu."
+                    break
+                    ;;
+                *)
+                    echo "Invalid option. Please select 1, 2, 3, 4, or 5."
+                    ;;
+            esac
+        done
     else
         echo "Country not found or misspelled. Please check the spelling and try again."
     fi
