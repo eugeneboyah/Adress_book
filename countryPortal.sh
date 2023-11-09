ddad19e9ae12e5325b91eeae6a09c13db2c38aa2
eugeneboyah <142409518+eugeneboyah@users.noreply.github.com>
2023-11-09 08:52:35 +0000

    Add files via upload

--- countryPortal.sh
+++ countryPortal.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+declare -A search_history
 
 echo "Please enter your name:"
+echo
 read userName
 echo "Hello, $userName, and welcome to our Country Information Portal"
+echo 
 
 while true; do
     read -p "Enter a country name that you want to search (or 'exit' to quit): " country_name
@@ -31,27 +33,55 @@ while true; do
 
             case $option in
                 1)
-                    echo "Capital City: $(echo $country_info | cut -d',' -f2)"
+                    capital_city=$(echo $country_info | cut -d',' -f2)
+                    echo "Capital City: $capital_city"
+                    search_history["$country_name, Capital City"]=$capital_city
+                    echo
                     ;;
+                    
                 2)
-                    echo "Currency: $(echo $country_info | cut -d',' -f3)"
+                    currency=$(echo $country_info | cut -d',' -f3)
+                    echo "Currency: $currency"
+                    search_history["$country_name, Currency"]=$currency
+                    echo
                     ;;
+                    
                 3)
-                    echo "Official Language: $(echo $country_info | cut -d',' -f4)"
+                    language=$(echo $country_info | cut -d',' -f4)
+                    echo "Official Language: $language"
+                    search_history["$country_name, Official Language"]=$language
+                    echo
                     ;;
+                    
                 4)
-                    echo "Head of Government: $(echo $country_info | cut -d',' -f5)"
+                    head_of_govt=$(echo $country_info | cut -d',' -f5)
+                    echo "Head of Government: $head_of_govt"
+                    search_history["$country_name, Head of Government"]=$head_of_govt
+                    echo
                     ;;
+                    
                 5)
                     echo "Returning to the main menu."
                     break
+                    echo
                     ;;
+                    
                 *)
                     echo "Invalid option. Please select 1, 2, 3, 4, or 5."
+                    echo
                     ;;
+                    
             esac
         done
     else
         echo "Country not found or misspelled. Please check the spelling and try again."
+        echo
     fi
+done
+
+# Print search history using AWK
+echo "Search History:"
+echo 
+for key in "${!search_history[@]}"; do
+    echo "$key: ${search_history[$key]}"
 done
